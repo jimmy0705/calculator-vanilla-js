57467574555d51a152ede85daaabdfa5a94ac7fe
juned123 <juned@frrole.com>
2020-07-13 15:46:38 +0530

    calculator all  functionality etc.

---
+++ script.js
@@ -0,0 +1,42 @@
+//console.log("connected");
+
+let screen = document.querySelector('.screen');
+
+let buttons = document.querySelectorAll('button');
+
+//console.log(buttons);
+
+//console.log(Array.from(buttons));
+let screenInput = '';
+
+buttons.forEach(btn =>{
+ //   console.log(btn);
+ btn.addEventListener('click',(event)=>{
+    //console.log(event.target.innerText);
+    let btnTxt = event.target.innerText;
+    
+   
+    if(btnTxt == 'CE'){
+    
+        screenInput ='';
+        screen.innerText = screenInput ;
+        
+
+    }
+    else if(btnTxt == '='){
+       
+        let result = eval(screenInput);
+        screenInput = result;
+        screen.innerText = screenInput;
+
+        console.log(result);
+    }else{
+    
+        screenInput += btnTxt;
+        screen.innerText = screenInput ;
+        console.log(screenInput);
+    
+     
+    }
+ });
+})
